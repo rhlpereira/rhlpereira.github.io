57071bcaa06f949447f1de6a676140f4af2526ed
Rafael Hanke <hanke@outlook.com>
2014-11-23 14:07:22 -0200

    Adicionados curriculos relacionados

---
+++ script/curriculo.js
@@ -0,0 +1,45 @@
+$('document').ready(function () {
+    $.support.cors = true;
+
+    $.ajax({
+        url: 'http://rarolabs.com.br:88/alunos.json',
+        dataType: 'json',
+
+        success: function (data) {
+            
+            data.sort(function (a, b) {
+                if (a.nome > b.nome)
+                    return 1;
+                
+                if (a.nome == b.nome)
+                    return 0;
+                
+                if (a.nome > b.nome)
+                    return -1;
+            });
+
+            $('document').delay(1000);
+
+            data.forEach(function (e, i) {
+                var link;
+
+                if (e.link_html != "") {
+                    link = e.link_html
+                } else {
+                    link = "#"
+                }
+
+                if (e.nome.toLowerCase().indexOf("rafael hanke") <= 0)
+                {
+                    $("#divRelacionados").append("\n <a href='" + link + "' class='list-group-item'><h4 class='list-group-item-heading'>" + e.nome + "</h4><p class='list-group-item-text'>" + e.mini_curriculo + "</p></a>");
+                }
+            });
+
+            $("#divAlerta").remove();
+        },
+
+        error: function (xhr, status, errorThrown) {
+            console.log(errorThrown + '\n' + status + '\n' + xhr.statusText);
+        }
+    });
+})
